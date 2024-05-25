a16beb6487ffeab6b7357dfaef942b6a6b6e27d4
Derek Stampone <dstampone@schools.nyc.gov>
2024-05-25 19:58:51 -0400

    Updates

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,22 @@
 import os
 
-from flask import render_template, request, Blueprint, redirect, url_for, flash, current_app, session
+from flask import (
+    render_template,
+    request,
+    Blueprint,
+    redirect,
+    url_for,
+    flash,
+    current_app,
+    session,
+)
 from werkzeug.utils import secure_filename
 
 import app.main.forms as report_forms
-from app.scripts.programming.class_lists import generate_class_list
-from app.scripts.surveys.connect_google_survey_with_class_lists import connect_google_survey_with_class_lists
 
 import app.scripts.utils as utils
 import app.scripts.update_from_jupiter as update_from_jupiter
+
 files_df = utils.return_dataframe_of_files()
 gsheets_df = utils.return_dataframe_of_gsheets()
 
@@ -36,17 +44,19 @@ def return_index():
 
 @main.route("/view/")
 def view_all_reports():
-    
+
     data = {
-            'reports':[
-                {'html':files_df.to_html(classes=["table", "table-sm"]),
-                'title':'View Files'
-                },
-                {'html':gsheets_df.to_html(classes=["table", "table-sm"]),
-                'title':'View Gsheets'
-                },
-            ]
-        }
+        "reports": [
+            {
+                "html": files_df.to_html(classes=["table", "table-sm"]),
+                "title": "View Files",
+            },
+            {
+                "html": gsheets_df.to_html(classes=["table", "table-sm"]),
+                "title": "View Gsheets",
+            },
+        ]
+    }
     return render_template("viewReport.html", data=data)
 
 
@@ -57,17 +67,19 @@ def view_most_recent_report(report):
     report_html = report_df.to_html(classes=["table", "table-sm"])
     return render_template("viewReport.html", report_html=report_html)
 
-@main.route("/update_from_jupiter", methods=["GET","POST"])
+
+@main.route("/update_from_jupiter", methods=["GET", "POST"])
 def return_update_from_jupiter():
     form = report_forms.JupiterUpdateForm()
     if form.validate_on_submit():
         report = form.report.data
         year_and_semester = form.year_and_semester.data
-        update_from_jupiter.main(report,year_and_semester)
+        update_from_jupiter.main(report, year_and_semester)
         flash(f"{report} successfully uploaded", category="success")
         return redirect(url_for("main.return_index"))
     else:
         return render_template("updateJupiter.html", form=form)
+
 
 @main.route("/upload_gsheet", methods=["GET", "POST"])
 def upload_gsheet():
@@ -88,26 +100,31 @@ def upload_gsheet():
         }
 
         from csv import DictWriter
- 
+
         # list of column names
-        field_names = ['gsheet_url', 'gsheet_category', 'year_and_semester',
-                    'school_year', 'semester']
-        
+        field_names = [
+            "gsheet_url",
+            "gsheet_category",
+            "year_and_semester",
+            "school_year",
+            "semester",
+        ]
+
         # Open CSV file in append mode
         # Create a file object for this file
         gsheet_urls_csv_filepath = os.path.join(
             current_app.root_path, f"data/gsheet_urls.csv"
         )
-        with open(gsheet_urls_csv_filepath, 'a') as f_object:
-        
+        with open(gsheet_urls_csv_filepath, "a") as f_object:
+
             # Pass the file object and a list
             # of column names to DictWriter()
             # You will get a object of DictWriter
             dictwriter_object = DictWriter(f_object, fieldnames=field_names)
-        
+
             # Pass the dictionary as an argument to the Writerow()
             dictwriter_object.writerow(file_dict)
-        
+
             # Close the file object
             f_object.close()
 
@@ -115,6 +132,7 @@ def upload_gsheet():
         return redirect(url_for("main.upload_gsheet"))
 
     return render_template("uploadGsheet.html", form=form)
+
 
 @main.route("/upload", methods=["GET", "POST"])
 def upload_files():
@@ -124,7 +142,7 @@ def upload_files():
         f = form.file.data
         filename = secure_filename(f.filename)
         filename = filename.replace("_", "-")
-        if filename.count('.')>2:
+        if filename.count(".") > 2:
             report_name = filename.split(".")[1]
             extension = filename.split(".")[-1]
             filename = f"{report_name}.{extension}"
@@ -137,7 +155,7 @@ def upload_files():
 
         download_date = form.download_date.data
         year_and_semester = form.year_and_semester.data
-        if report_name=='attendance':
+        if report_name == "attendance":
             filename = f"{year_and_semester}_9999-12-31_{filename}"
         else:
             filename = f"{year_and_semester}_{download_date}_{filename}"
@@ -154,15 +172,16 @@ def upload_files():
 
     return render_template("upload.html", form=form)
 
-@main.route("/setsemester",methods=["POST"])
+
+@main.route("/setsemester", methods=["POST"])
 def set_semester():
     semester = request.form.get("semester")
-    school_year, term = semester.split('-')
+    school_year, term = semester.split("-")
 
-    session['semester'] = semester
+    session["semester"] = semester
     session["school_year"] = int(school_year)
     session["term"] = int(term)
     session.permanent = True
 
-    flash(f'Semester set to {semester}')
+    flash(f"Semester set to {semester}")
     return redirect(request.referrer)
