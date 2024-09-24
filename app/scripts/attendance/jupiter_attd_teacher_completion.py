fdbbf5ba8f925c38d772da6e65381395d9676eac
Derek Stampone <dstampone@schools.nyc.gov>
2024-09-24 12:28:22 -0400

    Update general attendance scrips

--- app/scripts/attendance/jupiter_attd_teacher_completion.py
+++ app/scripts/attendance/jupiter_attd_teacher_completion.py
@@ -11,17 +11,22 @@ def main():
 
     term = session["term"]
 
-    ## Analyze attendance
-    jupiter_attd_filename = utils.return_most_recent_report(files_df, "jupiter_period_attendance")
-    attendance_marks_df = utils.return_file_as_df(jupiter_attd_filename)
+    school_year = session["school_year"]
+    term = session["term"]
 
+    year_and_semester = f"{school_year}-{term}"
+    
+    ## Analyze attendance
+    jupiter_attd_filename = utils.return_most_recent_report_by_semester(files_df, "jupiter_period_attendance",year_and_semester=year_and_semester)
+    attendance_marks_df = utils.return_file_as_df(jupiter_attd_filename)
+    print(attendance_marks_df)
     ## convert date and insert marking period
     attendance_marks_df["Date"] = pd.to_datetime(attendance_marks_df["Date"])
     attendance_marks_df["Term"] = attendance_marks_df["Date"].apply(return_mp_from_date, args=(school_year,))
     ## keep current semester
     attendance_marks_df = attendance_marks_df[attendance_marks_df["Term"].str[1]==str(term)]
     ## aggregate attendace by teacher, date, Course, Section
-
+    print(attendance_marks_df)
     df = pd.pivot_table( 
         attendance_marks_df[attendance_marks_df['Type']=='present'],
         index=['Teacher','Course','Section'],
